b99f8e9b601755814282156a60a9368c13f996f9
kpartusch <partuschk@gmail.com>
2018-12-02 18:02:33 -0800

    Added processing of two number functions

--- server/calculator/calculator.controller.spec.ts
+++ server/calculator/calculator.controller.spec.ts
@@ -7,14 +7,88 @@ import { app } from '../app';
 chai.use(require('chai-http')).should();
 
 describe('Calculator Controller', () => {
-  it('should calculate a result when valid inputs provided', done => {
+  let firstOperand: number;
+  let secondOperand: number;
+  const endPointUrl = '/api/calculator';
+
+  it('should return zero when inputs array is empty', (done) => {
     chai.request(app)
-      .get('/api/calculator')
+      .get(endPointUrl)
+      .send({inputs: []})
       .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        res.body.should.have.a.property('data').eql('Hello world');
+        assertSuccessResponse(res, 0);
         done();
       });
   });
+
+  describe('two number functions', () => {
+    it('should calculate a result when addition', (done) => {
+      firstOperand = 3;
+      secondOperand = 2;
+      chai.request(app)
+        .get(endPointUrl)
+        .send({inputs: [firstOperand.toString(), secondOperand.toString(), '+']})
+        .end((err, res) => {
+          assertSuccessResponse(res, firstOperand + secondOperand);
+          done();
+        });
+    });
+
+    it('should calculate a result when subtraction', (done) => {
+      firstOperand = 20;
+      secondOperand = 9.5;
+      chai.request(app)
+        .get(endPointUrl)
+        .send({inputs: [firstOperand.toString(), secondOperand.toString(), '-']})
+        .end((err, res) => {
+          assertSuccessResponse(res, firstOperand - secondOperand);
+          done();
+        });
+    });
+
+    it('should calculate a result when multiplication', (done) => {
+      firstOperand = 8;
+      secondOperand = 20;
+      chai.request(app)
+        .get(endPointUrl)
+        .send({inputs: [firstOperand.toString(), secondOperand.toString(), '*']})
+        .end((err, res) => {
+          assertSuccessResponse(res, firstOperand * secondOperand);
+          done();
+        });
+    });
+
+    it('should calculate a result when division', done => {
+      firstOperand = 14;
+      secondOperand = 2;
+      chai.request(app)
+        .get(endPointUrl)
+        .send({inputs: [firstOperand.toString(), secondOperand.toString(), '/']})
+        .end((err, res) => {
+          assertSuccessResponse(res, firstOperand / secondOperand);
+          done();
+        });
+    });
+
+    it('should return error when inputs array has invalid sequence', (done) => {
+      firstOperand = 3;
+      secondOperand = 2;
+      chai.request(app)
+        .get(endPointUrl)
+        .send({inputs: [firstOperand.toString(), '*', secondOperand.toString()]})
+        .end((err, res) => {
+          res.should.have.status(422);
+          res.body.should.be.a('object');
+          res.body.should.have.a.property('errors')
+            .eql([{message: 'inputs provided do not represent a valid postfix (RPN) expression'}]);
+          done();
+        });
+    });
+  });
+
+  function assertSuccessResponse(res: any, result: number): void {
+    res.should.have.status(200);
+    res.body.should.be.a('object');
+    res.body.should.have.a.property('data').eql(result);
+  }
 });
--- server/calculator/calculator.controller.ts
+++ server/calculator/calculator.controller.ts
@@ -6,5 +6,36 @@ import { Response, Request, NextFunction } from 'express';
  * either operands (numbers) or operators.
  */
 export function getCalculatorResult(req: Request, res: Response) {
-  return res.status(200).json({ data: 'Hello world'});
+  const inputs = <string[]>req.body.inputs;
+  const stack: number[] = [];
+  inputs.forEach((value) => {
+    if (['+', '-', '*', '/'].includes(value)) {
+      // two number functions cause stack drop
+      const operand_x = stack.pop();
+      const operand_y = stack.pop();
+      const twoNumberResult = evaluate(operand_y, operand_x, value);
+      if (isNaN(twoNumberResult)) {
+        return res.status(422).json({ errors: [ { message: 'inputs provided do not represent a valid postfix (RPN) expression' }]});
+      }
+      stack.push(evaluate(operand_y, operand_x, value));
+    } else if (!isNaN(+value)) {
+      stack.push(+value);
+    }
+  });
+
+  const result = stack.length === 0 ? 0 : stack.pop();
+  res.status(200).json({ 'data': result });
+}
+
+function evaluate(leftOperand: number, rightOperand: number, operator: string): number {
+  switch (operator) {
+    case '+':
+      return leftOperand + rightOperand;
+    case '-':
+      return leftOperand - rightOperand;
+    case '*':
+      return leftOperand * rightOperand;
+    case '/':
+      return leftOperand / rightOperand;
+  }
 }
